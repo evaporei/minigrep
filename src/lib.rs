3cded16ac26f5d3973940e39e418158375a36b91
Otávio Pace <otaviopp8@gmail.com>
2018-02-24 04:44:06 -0300

    extract code in main.rs to lib.rs
    Extracted:
    - Config struct
    - Config::new associated function
    - run function

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub file_name: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Config { query, file_name })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut file = File::open(config.file_name)?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,37 +15,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_name);
 
-    if let Err(err) = run(config) {
+    if let Err(err) = minigrep::run(config) {
         println!("Application error: {}", err);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut file = File::open(config.file_name)?;
-
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_name: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_name = args[2].clone();
-
-        Ok(Config { query, file_name })
     }
 }
