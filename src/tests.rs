575b538c8e4e69087c76a6fca7b5ce230fcd9217
Otávio Pace <otaviopp8@gmail.com>
2018-02-24 05:17:59 -0300

    lib: add tests to run function

--- src/tests.rs
+++ src/tests.rs
@@ -27,3 +27,28 @@ fn config_new_should_return_error() {
 
     Config::new(&args).unwrap();
 }
+
+#[test]
+fn run_should_return_ok() {
+    let args = vec![
+        "minigrep".to_string(),
+        "bla".to_string(),
+        "poem.txt".to_string(),
+    ];
+
+    let config = Config::new(&args).unwrap();
+    run(config).expect("to run without returning an Error");
+}
+
+#[test]
+#[should_panic]
+fn run_should_return_error() {
+    let args = vec![
+        "minigrep".to_string(),
+        "bla".to_string(),
+        "non_exiting_file.txt".to_string(),
+    ];
+
+    let config = Config::new(&args).unwrap();
+    run(config).unwrap();
+}
