f8bbde15b66dd7742265a1ee879755323970cd7e
Otávio Pace <otaviopp8@gmail.com>
2018-02-24 09:55:25 -0300

    functions: refactor search functions using filter

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+        println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(config) {
-        eprintln!("Application error: {}", err);
+        println!("Application error: {}", err);
         process::exit(1);
     }
 }
