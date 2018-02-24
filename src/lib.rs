0485f2602aa817051dafc6111e49edc53de933b9
Otávio Pace <otaviopp8@gmail.com>
2018-02-24 07:42:20 -0300

    functions: make run use search

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,9 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
