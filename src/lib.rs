656a3a3d591d247172e99346ef66558cc1718c1e
Otávio Pace <otaviopp8@gmail.com>
2018-02-24 07:37:20 -0300

    functions: add search, main functionality of grep

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,18 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut file = File::open(config.file_name)?;
 
--- src/tests/functions.rs
+++ src/tests/functions.rs
@@ -24,3 +24,33 @@ fn run_should_return_error() {
     let config = Config::new(&args).unwrap();
     run(config).unwrap();
 }
+
+#[test]
+fn search_should_return_one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(
+        vec!["safe, fast, productive."],
+        search(query, contents)
+    );
+}
+
+#[test]
+fn search_should_return_no_result() {
+    let query = "not in content";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    let results: Vec<&str> = Vec::new();
+
+    assert_eq!(
+        results,
+        search(query, contents)
+    );
+}
